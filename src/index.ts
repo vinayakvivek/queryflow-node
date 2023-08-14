e2c685bbc093097a68e1120e33f7ec9dd00a6d3e
Vinayak <vinayakdeepu@gmail.com>
2023-08-14 10:33:03 +0530

    Move QueryflowClient into client.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
-import {
-  IQueryflowClient,
-  QueryResponse,
-  QueryflowClientOptions,
-} from "./types";
-
-export class QueryflowClient implements IQueryflowClient {
-  baseUrl: string;
-  apiKey: string;
-
-  constructor(options: QueryflowClientOptions) {
-    this.baseUrl = options.baseUrl || "https://api.queryflow.com/v1";
-    this.apiKey = options.apiKey;
-  }
-
-  async query(queryText: string): Promise<QueryResponse> {
-    return { text: "mock" };
-  }
-}
+export * from "./client";
+export * from "./types";
